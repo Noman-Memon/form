3738c93df29fda46dfc8103f580b09e0b6726338
ABASITJAMALBROTHERS <noman.rauf@jamalbrothers.com.pk>
2022-11-21 15:18:41 +0500

    add  textArea

--- src/Form.js
+++ src/Form.js
@@ -5,9 +5,10 @@ export default function Form() {
     firstName: '',
     lastName: '',
     email: '',
+    textArea: '',
   })
 
-  console.log(formData)
+  console.log(formData.textArea)
 
   function handleChange(event) {
     setFormData((prevFormData) => {
@@ -25,18 +26,27 @@ export default function Form() {
         placeholder="First Name"
         onChange={handleChange}
         name="firstName"
+        value={formData.firstName} //--by this- now state control this input field rahter input field control state
       />
       <input
         type="text"
         placeholder="Last Name"
         onChange={handleChange}
         name="lastName"
+        value={formData.lastName}
       />
       <input
         type="email"
         placeholder="Email"
         onChange={handleChange}
         name="email"
+        value={formData.email}
+      />
+      <textarea
+        placeholder="Type here"
+        onChange={handleChange}
+        name="textArea"
+        value={formData.textArea}
       />
     </form>
   )
