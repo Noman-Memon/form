181c1cf784146e1781fbc3284c31c7227a382428
ABASITJAMALBROTHERS <noman.rauf@jamalbrothers.com.pk>
2022-11-22 12:33:55 +0500

    add select option

--- src/Form.js
+++ src/Form.js
@@ -8,6 +8,7 @@ export default function Form() {
     textArea: '',
     checkBox: false,
     employment: '',
+    favColor: '',
   })
 
   console.log(formData.checkBox)
@@ -105,6 +106,23 @@ export default function Form() {
         />
         <label htmlFor="full-time">full-time</label>
       </fieldset>
+      <label htmlFor="favColor">What is your favorite color?</label>
+      <br />
+      <select
+        id="favColor"
+        value={formData.favColor}
+        onChange={handleChange}
+        name="favColor"
+      >
+        <option value="">-- Choose --</option>
+        <option value="red">Red</option>
+        <option value="orange">Orange</option>
+        <option value="yellow">Yellow</option>
+        <option value="green">Green</option>
+        <option value="blue">Blue</option>
+        <option value="indigo">Indigo</option>
+        <option value="violet">Violet</option>
+      </select>
     </form>
   )
 }
